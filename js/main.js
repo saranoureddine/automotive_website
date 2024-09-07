8b825ef65eb28f046a1f1a1406800d481f17b6ae
Sara Noureddine <sarahnoureddin29@gmail.com>
2024-09-07 17:33:13 +0300

    adding  library

---
+++ js/main.js
@@ -0,0 +1,131 @@
+(function ($) {
+    "use strict";
+
+   
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').css('top', '0px');
+        } else {
+            $('.sticky-top').css('top', '-100px');
+        }
+    });
+    
+   
+    
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+$('.back-to-top').click(function () {
+    $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+    return false;
+});
+
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Date and time picker
+    $('.date').datetimepicker({
+        format: 'L'
+    });
+    $('.time').datetimepicker({
+        format: 'LT'
+    });
+
+// product carousel
+$(".product-carousel, .related-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    margin: 30,
+    dots: false,
+    loop: true,
+    nav : true,
+    navText : [
+        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    ],
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+});
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: true,
+        margin: 25,
+        dots: true,
+        loop: true,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+
+// Vendor carousel
+$('.vendor-carousel').owlCarousel({
+    loop: true,
+    margin: 30,
+    dots: true,
+    loop: true,
+    center: true,
+    autoplay: true,
+    smartSpeed: 1000,
+    responsive: {
+        0:{
+            items:2
+        },
+        576:{
+            items:3
+        },
+        768:{
+            items:4
+        },
+        992:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+});
+
